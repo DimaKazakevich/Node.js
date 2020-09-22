c652d68a287d85a3fa3bb76d7c9d8b2164d7a483
DimaKazakevich <kazak1629@gmail.com>
2020-09-22 10:49:03 +0300

    Refactor code of sd and sc command and add ss command.

--- Lab4-5/04-01.js
+++ Lab4-5/04-01.js
@@ -1,3 +1,14 @@
+/*
+sd x	oстановить сервер через x секунд; ввод новой команды отменяет действие  предыдущей sd и устанавливает новый отсчет; 
+ввод команды sd  без параметра отменяет остановку сервера.         
+
+sc  x	запустить функцию периодической фиксации состояния БД; периодичность выполнения функции  commit задается в секундах параметром x; 
+ввод команды sс  без параметра останавливает периодическое выполнение commit.        
+ 
+ss x 	запустить функцию сбора статистики, которая работает x секунд и собирает за этот промежуток   статистику о количестве выполненных запросов и фиксаций БД; 
+ввод команды ss  без параметра останавливает сбор статистики.     
+*/
+
 let http = require('http');
 let url = require('url');
 let fs = require('fs');
@@ -6,8 +17,11 @@ const { clearInterval } = require('timers');
 
 let db = new data.DB();
 
+let amountOfRequests = 0;
+
 db.on('GET', (request, response) => {
     console.log('DB.GET');
+    amountOfRequests++;
     if(typeof url.parse(request.url, true).query.id !== 'undefined') {
         let idToPut = +url.parse(request.url, true).query.id;
         console.log(JSON.stringify(db.getById(idToPut)));
@@ -19,6 +33,7 @@ db.on('GET', (request, response) => {
 
 db.on('POST', (req, resp) => {
     console.log('Db.POST');
+    amountOfRequests++;
     req.on('data', data => {
         let reqData = JSON.parse(data);
         db.post(reqData);
@@ -28,12 +43,14 @@ db.on('POST', (req, resp) => {
 
 db.on('DELETE', (req, resp) => {
     console.log('Db.DELETE');
+    amountOfRequests++;
     let idToDelete = +url.parse(req.url, true).query.id;
     resp.end(JSON.stringify(db.delete(idToDelete)));
 })
 
 db.on('PUT', (req, resp) => {
     console.log('Db.PUT');
+    amountOfRequests++;
     req.on('data', data => {
         let reqData = JSON.parse(data);
         db.update(reqData.name, reqData.bday);
@@ -51,29 +68,52 @@ http.createServer(function(req, resp) {
     }
 }).listen(8080);
 
+let timerIdSdCommand = null;
+let timerIdScCommand = null;
+let timerIdSsCommand = null;
+let amountOfCommits = 0;
+
 process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', () => {
     let chunk = null;
     let newArr = [];
-    var timer;
+
     while((chunk = process.stdin.read()) != null) {
         newArr = chunk.trim().split(' ');
         if(newArr[0] === 'sd') {
-            setTimeout(function() {
-                console.log('Shut down');
-                process.exit(0);
-            }, newArr[1]);
+            if(newArr.length === 2) {
+                clearTimeout(timerIdSdCommand);
+                console.log(`Server will be stopped in ${newArr[1]} sec.`);
+                timerIdSdCommand = setTimeout(() => {
+                    console.log('Server stopped.');
+                    process.exit(0);
+                }, newArr[1] * 1000);
+            } else if(newArr.length === 1) {
+                console.log('Server stop canceled.');
+                clearTimeout(timerIdSdCommand);
+            }
         }
         if(newArr[0] === 'sc') {
-            console.log(newArr.length);
             if(newArr.length === 2) {
-            timer =  setInterval(function() {
-                    db.commit();
-                    console.log(chunk);
-                }, newArr[1]);
-                console.log(timer);
+                timerIdScCommand =  setInterval(() => {
+                        db.commit();
+                        amountOfCommits++;
+                    }, newArr[1] * 1000);
             } else if(newArr.length === 1) {
-                clearInterval(timer);
+                console.log('Server auto commiting canceled.')
+                clearInterval(timerIdScCommand);
+            }
+        }
+        if(newArr[0] === 'ss') {
+            if(newArr.length === 2) {
+                timerIdSsCommand =  setTimeout(() => {
+                        console.log(`Server started collecting statistics. Result will be in ${newArr[1]} sec.`);
+                        console.log(`Amount of completed requests - ${amountOfRequests}`);
+                        console.log(`Amount of completed requests - ${amountOfRequests}`);
+                    }, newArr[1] * 1000);
+            } else if(newArr.length === 1) {
+                console.log('Server statistics collection has been stopped.')
+                clearInterval(timerIdSsCommand);
             }
         }
     }

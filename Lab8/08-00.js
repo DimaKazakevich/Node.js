482b6430626fa2b30c01d656e73cb77a31bc10b5
DimaKazakevich <kazak1629@gmail.com>
2020-10-27 19:33:16 +0300

    Finish developing a simple server that handles many demo requests.

--- Lab8/08-00.js
+++ Lab8/08-00.js
@@ -1,5 +1,9 @@
 let http = require('http');
 let url = require('url');
+let fs = require('fs');
+let mp = require('multiparty');
+const parseString = require('xml2js').parseString;
+const xmlbuilder = require('xmlbuilder');
 
 let server = http.createServer();
 
@@ -30,8 +34,23 @@ let http_handler = (req, res) => {
             res.end(`<h2>Sum: ${x + y}</h2> \n <h2>Sub: ${x - y}</h2> \n <h2>Mult: ${x * y}</h2> \n <h2>Division: ${x / y}</h2>`);
         } else {
             res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-            res.end(`<h2>${url.parse(req.uri).pathname}</h2> \n <h2>Parameters is not number</h2>`);
+            res.end(`<h2>${url.parse(req.url).pathname}</h2> \n <h2>Parameters is not number</h2>`);
         }
+    }
+
+
+    if(RegExp(/^\/parameter\/[0-9][0-9]\/[0-9][0-9]/).test(url.parse(req.url).pathname)) {
+        res.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
+        var p = url.parse(req.url, true);
+
+        if((x = +p.pathname.split('/')[2]) && (y = +p.pathname.split('/')[3])) {
+            res.end('<h1>X and Y are int</h1>'
+            + `<p>x = ${x}, y = ${y}</p>`
+            + `<p>x + y = ${x + y}</p>`
+            + `<p>x - y = ${x - y}</p>`
+            + `<p>x * y = ${x * y}</p>`
+            + `<p>x / y = ${x / y}</p>`
+        );} 
     }
     
     if(url.parse(req.url).pathname === '/close') {
@@ -44,7 +63,119 @@ let http_handler = (req, res) => {
 
     if(url.parse(req.url).pathname === '/socket') {
         res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-        res.end(`<h2>localAddress: ${clientSocket.localAddress} \n localPort ${clientSocket.localPort}</h2>`);
+        res.end(`<h2>localAddress: ${clientSocket.localAddress} \n localPort ${clientSocket.localPort} \n serverAddress ${JSON.stringify(server.address())}</h2>`);
+    }
+
+    if(url.parse(req.url).pathname === '/req-data') {
+        res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+        let buf = '';
+        req.on('data', (data) => {
+            console.log('requsent.on(data) = ', data.length);
+            buf += data;
+        });
+        req.on('end', () => {
+            console.log('request.on(end) = ', buf.length)
+        });
+        res.end('1');
+    }
+
+    
+    if(url.parse(req.url).pathname === '/resp-status') {
+        if((x = +url.parse(req.url, true).query.code) && (y = url.parse(req.url, true).query.mess))  {
+            res.writeHead(x,y, {'Content-Type': 'text/html; charset=utf-8'});
+            res.end();
+        }
+    }
+
+    if(url.parse(req.url).pathname === '/formparameter') {
+        if (url.parse(req.url).pathname === '/formparameter' && req.method === 'POST') {
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            let data = '';
+            req.on('data', chunk => { data += chunk; });
+            req.on('end', () => {
+                console.log(data);
+                res.end(data);
+            });
+        }
+        if (url.parse(req.url).pathname === '/formparameter' && req.method === 'GET') {
+            let html = fs.readFileSync('form.html');
+            res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+            res.end(html);
+        }
+    }
+
+    if (url.parse(req.url).pathname === '/json' && req.method === 'POST') {
+        let data = '';
+        req.on('data', (chunk) => { data += chunk; });
+        req.on('end', () => {
+            res.writeHead(200, {'Content-type': 'application/json; charset=utf-8'});
+            data = JSON.parse(data);
+            let jsonResponse = {};
+            jsonResponse.__comment = 'Response: ' + data.__comment;
+            jsonResponse.x_plus_y = data.x + data.y;
+            jsonResponse.Concatenation_s_o = data.s + ': ' + data.o.surname + ', ' + data.o.name;
+            jsonResponse.Length_m = data.m.length;
+            res.end(JSON.stringify(jsonResponse));
+        });
+    }
+
+    if (url.parse(req.url).pathname === '/xml' && req.method === 'POST') {
+        let data = '';
+        req.on('data', (chunk) => { data += chunk; });
+        req.on('end', () => {
+            res.writeHead(200, {'Content-type': 'application/xml'});
+            parseString(data, function(err, result) {
+                let id = result.request.$.id;
+                let xSum = 0;
+                let mSum = '';
+                result.request.x.forEach((p) => {
+                    xSum += parseInt(p.$.value);
+                });
+                result.request.m.forEach((p) => {
+                    mSum += p.$.value;
+                });
+
+                let xmlDoc = xmlbuilder.create('response').att('id', id);
+                xmlDoc.ele('sum').att('element', 'x').att('result', xSum).up().ele('concat').att('element', 'm').att('result', mSum);
+
+                res.end(xmlDoc.toString());
+            });
+        });
+    }
+
+    if (url.parse(req.url).pathname === '/files') {
+        res.setHeader('X-static-files-count', fs.readdirSync('./node_modules').length);
+        res.end();
+    }
+
+    if (RegExp(/^\/files\/[a-z]/).test(url.parse(req.url).pathname)) {
+        try {
+            let parsedUrl = url.parse(req.url, true);
+            let data = fs.readFileSync('static/' + parsedUrl.pathname.split('/')[2]);
+            res.end(data);
+        } catch (e) {
+            res.writeHead(404, {'Content-type': 'text/html'});
+            res.end('404 ' + e.toString());
+        }
+    }
+
+    if (url.parse(req.url).pathname === '/upload') {
+        if(req.method === 'POST') {
+            let form = new mp.Form({uploadDir: './static'});
+            form.on('file', (name, file) => { });
+            form.on('close', () => {
+                res.writeHead(200, {'Content-type': 'text/plain'});
+                res.end("Uploaded!");
+            });
+            form.parse(req);
+        }
+        if(req.method === 'GET') {
+            res.writeHead(200, {'Content-type': 'text/html'});
+            res.end('<form method="POST" action="/upload" enctype="multipart/form-data">' +
+                '<input type="file" name="file"/>' +
+                '<input type="submit"/>' +
+                '</form>');
+        }
     }
 }
 
